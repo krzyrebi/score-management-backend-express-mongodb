2a16f5f3623c5c39a96c8ab07969a03860dac318
krzy rebi <petarok44@gmail.com>
2023-02-23 16:25:14 +0100

    cors cd3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const router = require('./routes/api')
 require('./db/mongoose')
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
